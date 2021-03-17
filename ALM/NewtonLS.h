cc81c4ac54a687e0a5a326a2fd2e4fd75939bfd7
detu <eka.suwartadi@gmail.com>
2021-03-17 22:21:32 +0100

    Test NewtonLS class with a simple case.

--- ALM/NewtonLS.h
+++ ALM/NewtonLS.h
@@ -8,16 +8,19 @@ struct NewtonLS{
       double beta1  = 1.0e-4;
       double beta2  = 0.99;
       double lambda = 2;
-      double eps    = 1.0e-8;
+      double eps    = 1e-8;
       int    k      = 0;
-      Eigen::VectorXd xk = x0_;
-      Eigen::VectorXd g  = Q_*xk + b_;
-      while ( g.norm() >= eps || k <= maxIter_){
+      x_ = x0_;
+      Eigen::VectorXd g  = Q_*x_ + b_;
+      double gNorm = g.norm();
+      while ( gNorm > eps && k <= maxIter_){
         Eigen::VectorXd d = Q_.llt().solve(b_);
-          alpha = lineSearch(Q_,g,xk,d,alpha0,beta1,beta2,lambda);
-          xk += alpha * d;
-          g  = Q_*xk + b_;
-          k++;
+//        double alpha = lineSearch(Q_,b_,g,x_,d,alpha0,beta1,beta2,lambda);
+//        x_ -= alpha * d;
+        x_ -= d;
+        g  = Q_*x_ + b_;
+        k++;
+        gNorm = g.norm();
       }
   }
 
@@ -32,17 +35,17 @@ private:
   Eigen::VectorXd x0_;
   int maxIter_;
 
-  double lineSearch(Eigen::MatrixXd &Q, Eigen::VectorXd &g, Eigen::VectorXd &x, Eigen::VectorXd &d, double alpha0, double beta1, double beta2, double lambda){
+  double lineSearch(Eigen::MatrixXd &Q, Eigen::VectorXd &b,Eigen::VectorXd &g, Eigen::VectorXd &x, Eigen::VectorXd &d, double alpha0, double beta1, double beta2, double lambda){
       double alpha  = alpha0;
       double alphal = 0;
       double alphar = 999999;
       double deriv  = g.transpose()*d;
-      double f      = x.transpose()*Q*x + x.transpose()*b;
+      double f        = (x.transpose()*Q*x + x.transpose()*b).value();
       int ok = 0;
       while (ok == 0){
           Eigen::VectorXd xnew = x + alpha*d;
-          double fnew          = xnew.transpose()*Q*xnew + xnew.transpose()*b;
-          Eigen::VectorXd gnew = xnew.transpose()*Q + b;
+          double fnew          = (xnew.transpose()*Q*xnew + xnew.transpose()*b).value();
+          Eigen::VectorXd gnew = Q*xnew + b;
 
           if ( fnew > (f + alpha*beta1*deriv)){
               alphar = alpha;
@@ -58,5 +61,7 @@ private:
               ok = 1;
           }
       }
+
+      return alpha;
   }
 };
--- ALM/main.cpp
+++ ALM/main.cpp
@@ -1,10 +1,11 @@
-#include "DirectQP.h"
+//#include "DirectQP.h"
+#include "NewtonLS.h"
 #include <Eigen/Dense>
 #include <iostream>
 
 int main(){
-    Eigen::Matrix4f Q;
-    Eigen::Vector4f b;
+    Eigen::Matrix4d Q;
+    Eigen::Vector4d b;
     Q << 1,1,1,1,
          1,2,2,2,
          1,2,3,3,
@@ -14,9 +15,13 @@ int main(){
          -9,
          -10;
 
-    auto qp = DirectQP(Q,b);
+    //auto qp = DirectQP(Q,b);
+    Eigen::Vector4d x0{0,0,0,0};
+    auto qp = NewtonLS(Q,b,x0);
     auto x  = qp.solve();
+    auto fobj = (x.transpose()*Q*x + x.transpose()*b).value();
     std::cout << "The solution is:\n" << x << std::endl;
+    std::cout << "The optimal objective function value:\n" << fobj << std::endl;
 
     return 0;
 }

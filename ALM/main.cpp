9805e720a920a29fb07f46a75e0a96c38b73235d
detu <eka.suwartadi@gmail.com>
2021-05-09 01:12:24 +0200

    Read input from MATLAB.

--- ALM/main.cpp
+++ ALM/main.cpp
@@ -1,7 +1,9 @@
 #include "Function.h"
 #include "NewtonALM.h"
+#include "MATio"
 #include <iostream>
 #include <iomanip>
+#include <Eigen/Sparse>
 
 using FunctionXd = cppoptlib::function::Function<double>;
 
@@ -21,56 +23,99 @@ public:
     }
 
     void Gradient(const vector_t &x,  const matrix_t &H, const vector_t &f, const matrix_t &Aeq, const vector_t &beq, const vector_t &lambda, const scalar_t c, vector_t *grad) const override {
-        *grad = H * x + f + Aeq * lambda + c * Aeq * (Aeq*x - beq) ;
+        *grad = H * x + f + Aeq.transpose() * lambda + c * Aeq.transpose()* (Aeq*x - beq) ;
     }
 
     void Hessian(const vector_t &x,  const matrix_t &H, const vector_t &f, const matrix_t &Aeq, const vector_t &beq, const vector_t &lambda, const scalar_t c, hessian_t *hessian) const override {
-        *hessian = H + c*Aeq*Aeq;
+        *hessian = H + c*Aeq.transpose()*Aeq;
     }
 };
 
 int main(){
-    Eigen::Vector3d x0;
-    Eigen::Matrix3d H;
-    Eigen::Vector3d f;
-    Eigen::Matrix3d Aeq;
-    Eigen::Vector3d beq;
-    Eigen::Vector3d lb,ub;
-    Eigen::Vector3d lambda;
 
-    x0 << 0,
-          0,
-          0;
-    H << 5, -2, -1,
-         -2, 4, 3,
-         -1, 3, 5;
-    f << 2,
-         -35,
-         -47;
-
-//    Aeq.setZero();
-//    beq.setZero();
-//    lb.setZero();
-//    ub.setZero();
-    lambda.setZero();
-    Aeq << 1, 0, 0,
-           0, 1, 0,
-           0, 0, 0;
-    beq << 2,
-           3,
-           0;
-    lb << 0,
-          0,
-          0;
-    ub << 5,
-          5,
-          5;
+    matio::MatioFile file("qpData.mat");
+    //matio::MatioFile file("qpSparse.mat");
+    Eigen::MatrixXd H;
+    Eigen::VectorXd f;
+    Eigen::MatrixXd Aeq;
+    Eigen::VectorXd beq;
+    Eigen::VectorXd lb;
+    Eigen::VectorXd ub;
+    Eigen::VectorXd x0;
+    Eigen::VectorXd lambda;
+//    Eigen::SparseMatrix<double> H;
+//    Eigen::SparseMatrix<double> f;
+//    Eigen::SparseMatrix<double> Aeq;
+//    Eigen::SparseMatrix<double> beq;
+//    Eigen::SparseMatrix<double> lb;
+//    Eigen::SparseMatrix<double> ub;
 
 
+    if (file.read_mat("Hm", H)) {
+        std::cout << "error: " << file.lasterr() << std::endl;
+    }
+    if (file.read_mat("f", f)) {
+        std::cout << "error: " << file.lasterr() << std::endl;
+    }
+    if (file.read_mat("Aeq", Aeq)) {
+        std::cout << "error: " << file.lasterr() << std::endl;
+    }
+    if (file.read_mat("beq", beq)) {
+        std::cout << "error: " << file.lasterr() << std::endl;
+    }
+    if (file.read_mat("lb", lb)) {
+        std::cout << "error: " << file.lasterr() << std::endl;
+    }
+    if (file.read_mat("ub", ub)) {
+        std::cout << "error: " << file.lasterr() << std::endl;
+    }
+
+    int numX = Aeq.cols();
+    int numC = Aeq.rows();
+    x0.setZero(numX);
+    lambda.setZero(numC);
+
+
+//    Eigen::Vector3d x0;
+//    Eigen::Matrix3d H;
+//    Eigen::Vector3d f;
+//    Eigen::Matrix3d Aeq;
+//    Eigen::Vector3d beq;
+//    Eigen::Vector3d lb,ub;
+//    Eigen::Vector3d lambda;
+//
+//    x0 << 0,
+//          0,
+//          0;
+//    H << 5, -2, -1,
+//         -2, 4, 3,
+//         -1, 3, 5;
+//    f << 2,
+//         -35,
+//         -47;
+//
+////    Aeq.setZero();
+////    beq.setZero();
+////    lb.setZero();
+////    ub.setZero();
+//    lambda.setZero();
+//    Aeq << 1, 0, 0,
+//           0, 1, 0,
+//           0, 0, 0;
+//    beq << 2,
+//           3,
+//           0;
+//    lb << 0,
+//          0,
+//          0;
+//    ub << 5,
+//          5,
+//          5;
+//
+//
     Function fx;
     Function::scalar_t c(10);
     auto state = fx.Eval(x0, H, f, Aeq, beq, lb, ub, lambda, c);
-    std::cout << "this" << std::endl;
 
     cppoptlib::solver::NewtonBound<Function> solver;
 
@@ -107,37 +152,37 @@ int main(){
     double grad{100};
     double jac;
     int k = 0;
-    while(grad > eta && normX > eta) {
-        solver.setStoppingCriteria(epsilonk);
-        auto[solution, solver_state] = solver.Minimize(fx, x0, H, f, Aeq, beq,  lb, ub, lambda, c); // think how to supply stopping criteria here!
-        state = fx.Eval(solution.x, solution.H, solution.f, solution.Aeq, solution.beq, solution.lb, solution.ub, solution.lambda, solution.c);
-
-        // compute constraint value
-        cons = solution.Aeq * solution.x - solution.beq;
-        jac  = cons.norm();
-        if (jac <= etak){
-            lambda   = lambda + c*cons;
-            epsilonk = epsilonk/c;
-            etak     = etak / pow(c,beta);
-        } else {
-            c        = tau*c;
-            epsilonk = epsilon0/c;
-            etak     = eta0/pow(c,alpha);
-
-        }
-        //normX = (solution.x - x0).norm();
-        normX = (solution.x - x0).lpNorm<Eigen::Infinity>();
-        grad  = state.gradient.template lpNorm<Eigen::Infinity>();
-        x0    = solution.x;
-        if (verbose){
-            std::cout <<  k      << "\t" ;
-            std::cout << std::fixed << std::setprecision(4) << state.value << "\t" << "\t";
-            std::cout << std::fixed << std::setprecision(4) << grad << "\t" << "\t"  << "\t" << "\t" << "\t" << "\t";
-            std::cout << std::fixed << std::setprecision(4) << jac << "\t" << "\t" << "\t" << "\t";
-            std::cout << std::fixed << std::setprecision(4) << normX << std::endl;
-        }
-        k++;
-    }
+//    while(grad > eta && normX > eta) {
+//        solver.setStoppingCriteria(epsilonk);
+//        auto[solution, solver_state] = solver.Minimize(fx, x0, H, f, Aeq, beq,  lb, ub, lambda, c); // think how to supply stopping criteria here!
+//        state = fx.Eval(solution.x, solution.H, solution.f, solution.Aeq, solution.beq, solution.lb, solution.ub, solution.lambda, solution.c);
+//
+//        // compute constraint value
+//        cons = solution.Aeq * solution.x - solution.beq;
+//        jac  = cons.norm();
+//        if (jac <= etak){
+//            lambda   = lambda + c*cons;
+//            epsilonk = epsilonk/c;
+//            etak     = etak / pow(c,beta);
+//        } else {
+//            c        = tau*c;
+//            epsilonk = epsilon0/c;
+//            etak     = eta0/pow(c,alpha);
+//
+//        }
+//        //normX = (solution.x - x0).norm();
+//        normX = (solution.x - x0).lpNorm<Eigen::Infinity>();
+//        grad  = state.gradient.template lpNorm<Eigen::Infinity>();
+//        x0    = solution.x;
+//        if (verbose){
+//            std::cout <<  k      << "\t" ;
+//            std::cout << std::fixed << std::setprecision(4) << state.value << "\t" << "\t";
+//            std::cout << std::fixed << std::setprecision(4) << grad << "\t" << "\t"  << "\t" << "\t" << "\t" << "\t";
+//            std::cout << std::fixed << std::setprecision(4) << jac << "\t" << "\t" << "\t" << "\t";
+//            std::cout << std::fixed << std::setprecision(4) << normX << std::endl;
+//        }
+//        k++;
+//    }
 
     //std::cout << "argmin " << x0.transpose() << std::endl;
     return 0;
